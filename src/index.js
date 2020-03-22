0299ec388c962c30af44dfbffeb55f91ca4d5623
Sergey0s <mediaworld@mail.ru>
2020-03-22 10:38:59 +0300

    feat: add greater solution

--- src/index.js
+++ src/index.js
@@ -37,36 +37,55 @@ const MORSE_TABLE = {
     '-----': '0',
 };
 
-let phrase = [];
+// let phrase = [];
+//
+// function decode(expr) {
+//     phrase.length = 0;
+//     let dividedArr = [];
+//     const arr = expr.split('');
+//     for (let i = 0; i < arr.length / 10; i++) {
+//         dividedArr.push(arr.slice(i * 10, i * 10 + 10))
+//     }
+//     dividedArr.forEach(el => decodeOne(el));
+//     return phrase.join('')
+// }
+//
+// function decodeOne(dividedArr) {
+//     let arrWithout0 = dividedArr.slice(dividedArr.indexOf('1'));
+//     let wordArr = [];
+//     for (let i = 0; i < arrWithout0.length / 2; i++) {
+//         wordArr.push(arrWithout0.slice(i * 2, i * 2 + 2).join(''))
+//     }
+//     for (let item of wordArr) {
+//         if (item === '10') {
+//             wordArr[wordArr.indexOf(item)] = '.'
+//         } else if (item === '11') {
+//             wordArr[wordArr.indexOf(item)] = '-'
+//         } else {
+//             wordArr[wordArr.indexOf(item)] = '*'
+//         }
+//     }
+//     phrase.push(MORSE_TABLE[wordArr.join('')] || ' ')
+// }
+
 
 function decode(expr) {
-    phrase.length = 0;
-    let dividedArr = [];
-    const arr = expr.split('');
-    for (let i = 0; i < arr.length / 10; i++) {
-        dividedArr.push(arr.slice(i * 10, i * 10 + 10))
+
+    let codeArray = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        codeArray.push(expr.substring(i, i + 10));
     }
-    dividedArr.forEach(el => decodeOne(el));
-    return phrase.join('')
+    codeArray.map(item => (item.split("**********").join(" ")))
+        .map(item => (+item + ''))
+        .map(item => (item.split("11").join("-")))
+        .map(item => (item.split("10").join(".")))
+        .map(item => (item = MORSE_TABLE[item]))
+        .map(item => item === undefined ? ' ' : item)
+        .join("")
 }
 
-function decodeOne(dividedArr) {
-    let arrWithout0 = dividedArr.slice(dividedArr.indexOf('1'));
-    let wordArr = [];
-    for (let i = 0; i < arrWithout0.length / 2; i++) {
-        wordArr.push(arrWithout0.slice(i * 2, i * 2 + 2).join(''))
-    }
-    for (let item of wordArr) {
-        if (item === '10') {
-            wordArr[wordArr.indexOf(item)] = '.'
-        } else if (item === '11') {
-            wordArr[wordArr.indexOf(item)] = '-'
-        } else {
-            wordArr[wordArr.indexOf(item)] = '*'
-        }
-    }
-    phrase.push(MORSE_TABLE[wordArr.join('')] || ' ')
-}
+decode('0011101110000000101100101110100010111010**********000000001100101010100000000010**********001011111000001111110010111010000000101000111011100000000010')
 
 module.exports = {
     decode
